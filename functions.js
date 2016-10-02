06c938fefc96d672f4d436972521c0545f624080
saisthota <me@saisthota.com>
2016-10-01 20:58:41 -0700

    Added API check

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 var db = require('./db');
 var url = require('./models/urls.js');
+var client = require('./models/clients.js');
+var OID = db.Types.ObjectId;
 
 module.exports = {
     generateURL: function() {
@@ -15,9 +17,23 @@ module.exports = {
     checkURL: function(shortCode) {
         url.findOne({shortCode: shortCode}, function(err, data) {
             if(err) {
-                return '-1';
+                return -1;
             }
             
+            if(data) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+    },
+
+    checkAPI: function(key) {
+        client.findOne({_id: OID(key)}, function(err, data) {
+            if(err) {
+                return -1;
+            }
+
             if(data) {
                 return 1;
             } else {
